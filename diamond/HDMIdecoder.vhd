605ea35d60986f93b65b940b8597256c7b005a46
c0pperdragon <c0pperdragon50@gmail.com>
2025-12-30 22:29:12 +0100

    Send EDID

--- diamond/HDMIdecoder.vhd
+++ diamond/HDMIdecoder.vhd
@@ -156,15 +156,107 @@ begin
 		end if;
 	end process;
 
-	process (SCL)
-	variable toggle:boolean := false;
+    -- send EDID
+	process (SCL,SDA)
+	type Tedid is array (0 to 255) of integer;
+	constant edid:Tedid := 
+	(  	16#00#,16#ff#,16#ff#,16#ff#,16#ff#,16#ff#,16#ff#,16#00#,                                                                    -- standard header
+		16#0d#,16#f0#,16#01#,16#00#,16#01#,16#00#,16#00#,16#00#,16#0a#,16#22#,                                                      -- manufacturer info
+		16#01#,16#03#,                                                                                                                -- EDID version
+		16#a0#,                                                                                                                       -- video input parameters
+		16#28#,16#1e#,                                                                                                                -- screen size in cm
+		16#78#,                                                                                                                       -- gamma factor
+		16#06#,                                                                                                                       -- supported features bitmap    
+		16#EE#,16#91#,16#A3#,16#54#,16#4C#,16#99#,16#26#,16#0F#,16#50#,16#54#,                                                      -- chromaticity coordinates 
+		16#00#,16#00#,16#00#,                                                                                                         -- established modes
+		16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,16#01#,           -- standard modes
+		16#8c#,16#0a#,16#a0#,16#20#,16#51#,16#20#,16#18#,16#10#,16#18#,16#80#,16#33#,16#00#,16#90#,16#2c#,16#11#,16#00#,16#00#,16#18#, -- detailed timing descriptor: 14416#288
+		16#00#,16#00#,16#00#,16#fc#,16#00#,16#48#,16#44#,16#4d#,16#49#,16#32#,16#53#,16#43#,16#41#,16#52#,16#54#,16#0a#,16#20#,16#20#, -- monitor information
+		16#00#,16#00#,16#00#,16#10#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#, -- dummy
+		16#00#,16#00#,16#00#,16#10#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#, -- dummy
+		16#01#,                                                                                                                           -- number of extension blocks
+		16#00#,                                                                                                                           -- place for checksum
+		16#02#,16#03#,16#10#,16#40#,                                                                                                     -- header: support basic audio, 0 native formats
+		16#23#,16#09#,16#07#,16#07#,                                                                                                     -- audio data block
+		16#83#,16#01#,16#20#,16#20#,                                                                                                     -- speaker allocation
+		16#63#,16#03#,16#0c#,16#00#,                                                                                                     -- IEEE registration identifier
+		16#00#,16#00#,16#00#,16#00#,
+		16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,
+		16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,
+		16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,
+		16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,
+		16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,
+		16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#,16#00#  
+	);
+	type Tstate is (idle, address, ack, transfer);
+	variable state: Tstate := idle;
+	variable scl_on_falling_sda:std_logic := '0';
+	variable sda_on_rising_scl:std_logic := '0';
+	variable writebuffer:std_logic_vector(6 downto 0);
+	variable readbuffer:std_logic_vector(7 downto 0);
+	variable bitcount:integer range 7 downto 0;
+	variable readmode:boolean;
+	variable index:integer range 0 to 255 := 0;
+	variable isfirstbyte:boolean;
+	variable out_sda:std_logic := '1';
 	begin
+		if falling_edge(SDA) then
+			scl_on_falling_sda := SDA;
+		end if;
+		if rising_edge(SCL) then
+			sda_on_rising_scl := SCL;
+		end if;
 		if falling_edge(SCL) then
-			toggle := not toggle;
-			if toggle then 
-				SDA <= '0';
+			SDA <= 'Z';
+			-- detect a previous start condition
+			if SDA='0' and scl_on_falling_sda='1' then   
+				state := address;
+				bitcount := 0;
 			else
-				SDA <= 'Z';
+				-- state machine
+				case state is
+				when address =>
+					if bitcount<7 then
+						writebuffer := writebuffer(5 downto 0) & sda_on_rising_scl;
+						bitcount := bitcount+1;
+					elsif writebuffer = "1010000" then
+						readmode := sda_on_rising_scl='1';
+						state := ack;
+						SDA <= '0';   -- ack after matching address
+					else
+						state := idle;
+					end if;
+				when ack =>
+					state := transfer;
+					isfirstbyte := true;
+					bitcount := 0;
+					if readmode then
+						readbuffer := std_logic_vector(to_unsigned(edid(index), 8));
+						SDA <= readbuffer(7);
+						readbuffer := readbuffer(6 downto 0) & '0';
+					end if;
+				when transfer =>
+					if bitcount<7 then
+						writebuffer := writebuffer(5 downto 0) & sda_on_rising_scl;
+						bitcount := bitcount+1;
+						if readmode then
+							SDA <= readbuffer(7);
+							readbuffer := readbuffer(6 downto 0) & '0';
+						end if;
+					else
+						if isfirstbyte and not readmode then
+							index := to_integer(signed(writebuffer & sda_on_rising_scl));
+						else
+							index := index+1;
+						end if;
+						if not readmode then
+							SDA <= '0';  -- ack when writing to slave
+						end if;
+						isfirstbyte := false;
+						state := ack;
+					end if;				
+				when others =>
+				end case;			
 			end if;
 		end if;
 	end process;
